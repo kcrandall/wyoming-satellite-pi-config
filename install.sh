39c9b8d674849d7522616aa69c20679787226e8d
Keston Crandall <k_man@me.com>
2025-01-05 19:43:17 -0500

    misspelling

--- install.sh
+++ install.sh
@@ -20,14 +20,14 @@ sudo cp config/config.yaml /usr/local/bin/wyoming/
 sudo cp scripts/setup.py /usr/local/bin/wyoming/
 sudo cp scripts/configure.py /usr/local/bin/wyoming/
 sudo cp scripts/service_setup.sh /usr/local/bin/wyoming/
-sudo cp scripts/run-wakword.sh /usr/local/bin/wyoming/
+sudo cp scripts/run-wakeword.sh /usr/local/bin/wyoming/
 sudo cp scripts/run-satellite.sh /usr/local/bin/wyoming/
 
 # Make scripts executable
 echo "Making scripts executable..."
 sudo chmod +x /usr/local/bin/wyoming/setup.py
 sudo chmod +x /usr/local/bin/wyoming/service_setup.sh
-sudo chmod +x /usr/local/bin/wyoming/run-wakword.sh
+sudo chmod +x /usr/local/bin/wyoming/run-wakeword.sh
 sudo chmod +x /usr/local/bin/wyoming/run-satellite.sh
 
 # Run service setup
